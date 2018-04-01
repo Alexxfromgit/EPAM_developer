c105dbf294c039a05ff47440decbbae2b0b713e0
Alexx <d5enemy@gmail.com>
2018-04-01 17:20:41 +0300

    algorythm codes push

---
+++ Quadratic_equation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            double x = 0;
+            double x1 = 0;
+            double x2 = 0;
+
+            //Input values for quation
+            Console.WriteLine("Quadratic Equation like ax2 + bx + c = 0");
+            Console.Write("Input value a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input value b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input value c = ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            double d = Math.Pow(b, 2) - 4 * a * c;
+
+            if (d == 0)
+            {
+                x = -b / (2 * a);
+                Console.WriteLine("Answer x = " + x);
+            }
+            else if (d > 0)
+            {
+                x1 = (-b - Math.Sqrt(d)) / (2 * a);
+                x2 = (-b + Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("Answer x1 = " + x1 + ", Answer x2 = " + x2);
+            }
+            else
+            {
+                double real_root = -b / (2 * a);
+
+                double no_real_root = Math.Sqrt(-d) / (2 * a);
+
+                Console.WriteLine("Answer x1 = " + real_root + " + i * " + no_real_root);
+                Console.WriteLine("Answer x2 = " + real_root + " - i * " + no_real_root);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Shop_algorythm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input money value
+            double money = 0;
+
+            Console.WriteLine("Input how much money do you have.. ");
+            money = Convert.ToDouble(Console.ReadLine());
+
+            //Quantity of products
+            Console.WriteLine("Input quantity of products.. ");
+            int product_quantity = Convert.ToInt32(Console.ReadLine());
+
+            string product = " ";
+            int quantity = 0;
+            double price = 0;
+            double count = 0;
+
+            for (int i = 0; i < product_quantity; i++)
+            {
+                //Input products and count their summ
+                //Input name. String parameter
+
+                Console.WriteLine("Input what product do you wanna buy.. ");
+                product += Console.ReadLine() + " ";
+
+                //Input quantity. Int parameter
+                Console.WriteLine("Input quantity of product.. ");
+                quantity = Convert.ToInt32(Console.ReadLine());
+
+                //Input price. Double parameter
+                Console.WriteLine("Input price of product.. ");
+                price = Convert.ToDouble(Console.ReadLine());
+
+                //Counting summ
+                count += quantity * price;
+            }
+
+            //Calculating
+            double result = 0;
+
+            result = money - count;
+
+            if (result < 0)
+            {
+                Console.WriteLine("No enough money.. Sorry " + result);
+            }
+            else
+            {
+                Console.WriteLine("Your change is.. " + result);
+                Console.WriteLine("You will buy" + product);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
